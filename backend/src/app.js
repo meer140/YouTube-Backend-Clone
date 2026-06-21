a3d97276b26a12aca3fa35a0ec2ae5f601c32d53
meer140 <mirmustafa2005ali@gmail.com>
2026-06-21 14:51:43 +0500

    all setup configured

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,6 +14,10 @@ app.use(express.urlencoded({extended:true, limit : "16kb"}));
 app.use(express.static("public"))
 app.use(cookieParser());
 
+// import routes
+import router from "./routes/user.routes.js";
+
+app.use("/api/v1/users",router)
 
 
 export default app;
